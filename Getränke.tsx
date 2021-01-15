75eda7332d22256c9aca5f90725b38fb62b1ef01
simon1901 <77052619+simon1901@users.noreply.github.com>
2021-01-15 11:59:40 +0100

    This is a summary.

---
+++ Getränke.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="Getränke" tilewidth="32" tileheight="32" tilecount="432" columns="18">
+ <image source="../../../Downloads/Getränke etc..png" width="576" height="768"/>
+</tileset>
---
+++ Modern1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="Modern1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../Downloads/dbz1aoz-af48e951-df46-49da-873c-a85341406b30.png" width="512" height="512"/>
+</tileset>
