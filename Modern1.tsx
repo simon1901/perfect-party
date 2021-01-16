4568e694082f84ad75e9f6aad4e83277c7b034a3
simon1901 <77052619+simon1901@users.noreply.github.com>
2021-01-16 13:07:39 +0100

    This is a summary.

--- Modern1.tsx
+++ Modern1.tsx
@@ -1,4 +1,39 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="2020.12.18" name="Modern1" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../../../Downloads/dbz1aoz-af48e951-df46-49da-873c-a85341406b30.png" width="512" height="512"/>
+ <tile id="118">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="171">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
